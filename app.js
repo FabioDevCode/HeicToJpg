67e6ef6934b9444220ec4a6f85dc8eea221b0e9e
FabioDevCode <0677384849@orange.fr>
2023-05-20 16:13:43 +0200

    First Commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const fs = require('fs-extra');
+const { promisify } = require('util');
+const convert = require('heic-convert');
+
+const INPUT = './HEIC/';
+const OUTPUT = './JPG/';
+let nbfile = 0;
+let nbMaxFile;
+
+const getHeic = async() => {
+    try {
+        console.time('CONVERT HEIC TO JPG');
+        const files = await fs.readdir(INPUT);
+
+        if(!files.length) {
+            console.timeEnd('CONVERT HEIC TO JPG');
+            process.exit(1);
+        }
+
+        nbMaxFile = files.length;
+
+        await convertHeicToJpg(files);
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+}
+
+getHeic();
+
+const convertHeicToJpg = async(files) => {
+    if(!files.length) {
+        console.timeEnd('CONVERT HEIC TO JPG');
+        process.exit(1);
+    }
+
+    const filename = files[0];
+
+    try {
+        const inputBuffer = await promisify(fs.readFile)(INPUT+filename);
+
+        if(!inputBuffer) {
+            throw err;
+        }
+
+        const outputBuffer = await convert({
+            buffer: inputBuffer,
+            format: 'JPEG'
+        })
+
+        if(!outputBuffer) {
+            throw err;
+        }
+
+        await promisify(fs.writeFile)(`${OUTPUT+filename.split('.')[0]}.jpg`, outputBuffer);
+
+        nbfile++;
+        console.log(`${filename.split('.')[0]} convertie en JPG - ${nbfile}/${nbMaxFile}`);
+
+        // fs.unlink(INPUT+filename);
+
+        files.shift();
+        convertHeicToJpg(files);
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+}
